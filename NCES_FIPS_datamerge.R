7395ad5d93e50a6e824375bc64e160a1f991f199
kcamp27 <katherine.campbell2@emory.edu>
2021-04-18 17:42:29 -0400

    Uploaded file and fixed R code so Sanjana can add to MCH database

---
+++ NCES_FIPS_datamerge.R
@@ -0,0 +1,24 @@
+
+###############################################################
+######Creating School Directory for Public schools#############
+###############################################################
+#KC 4/18/2021
+library(dplyr)
+library(tidyr)
+library(forcats)
+library(readxl)
+library(ggplot2)
+library(here)
+library(stringr)
+
+#Read in excel files with public school districts and public schools from NCES database
+#https://nces.ed.gov/ccd/schoolsearch/school_list.asp?Search=1&State=41
+
+NCES <- read_excel('NCES Data/NCES_AL_GA_LA_MS_SC.xlsx')
+FIPS <- read_excel("NCES Data/FIPS_Codes.xlsx")
+
+FIPS <- FIPS %>% filter( State == "AL" | State == "GA" | State == "LA" | State == "MS" | State == "SC")
+
+NCES_FIPS <- merge(NCES, FIPS, by.x = c("County Name","State"), by.y = c("Name","State"))
+
+
