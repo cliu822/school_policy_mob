5f14b45ac56a5289e713fae9c5994b6125538297
kcamp27 <katherine.campbell2@emory.edu>
2021-04-18 14:45:01 -0400

    NCES Data for 5 states FIPS Codes for States Codes to merge NCES and FIPS

--- school_pol_or.R
+++ school_pol_or.R
@@ -122,3 +122,32 @@ df2 <- OR_totalsch %>%  filter(onsite == 1) %>% group_by(District) %>%
 summary(df2$perc_onsite_dist)
 
 
+
+
+
+###############################################################
+######Creating School Directory for Public schools#############
+###############################################################
+#KC 4/18/2021
+
+
+#Read in excel files with public school districts and public schools from NCES database
+#https://nces.ed.gov/ccd/schoolsearch/school_list.asp?Search=1&State=41
+
+NCES <- read_excel('NCES Data/NCES_AL_GA_LA_MS_SC.xlsx')
+FIPS <- read_excel("NCES Data/FIPS_Codes.xlsx")
+
+FIPS <- FIPS %>% filter( State == "AL" | State == "GA" | State == "LA" | State == "MS" | State == "SC")
+
+NCES_FIPS <- merge(NCES, FIPS, by.x = c("County Name","State"), by.y = c("Name","State"))
+
+
+
+
+
+
+
+
+
+
+
